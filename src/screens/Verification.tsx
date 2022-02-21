c23f080a6396d835aa3ce971f9aaf92b403ce9cf
developers-sensation <81740204+developers-sensation@users.noreply.github.com>
2022-02-21 12:38:44 +0530

    Otp functionality

--- src/screens/Verification.tsx
+++ src/screens/Verification.tsx
@@ -13,13 +13,12 @@ import {
   CodeField,
   Cursor,
   useBlurOnFulfill,
-  useClearByFocusCell,
-} from 'react-native-confirmation-code-field';
+  useClearByFocusCell
+} from "react-native-confirmation-code-field";
 
 interface VerificationProps {
   navigation: any;
   route: any;
-  setAuth: any;
 }
 
 interface VerificationState {
@@ -28,13 +27,12 @@ interface VerificationState {
 
 const CELL_COUNT = 4;
 
-const Verification = () => {
-
-  const [value, setValue] = useState('');
-  const ref = useBlurOnFulfill({value, cellCount: CELL_COUNT});
+const Verification = ({ navigation, route }: VerificationProps) => {
+  const [value, setValue] = useState("1234");
+  const ref = useBlurOnFulfill({ value, cellCount: CELL_COUNT });
   const [props, getCellOnLayoutHandler] = useClearByFocusCell({
     value,
-    setValue,
+    setValue
   });
 
   // constructor(props: any) {
@@ -43,115 +41,115 @@ const Verification = () => {
   //     otp: "1234"
   //   };
   // }
-  
+
   const otpHandler = (message: string) => {
-    // const otp = /(\d{4})/g.exec(message)[1];
-    // console.log("otp", otp);
-    // this.setState({ otp: "1234" }, this.verifyOtp);
-    // Keyboard.dismiss();
+    const otp = /(\d{4})/g.exec(message)[1];
+    console.log("otp", otp);
+    setValue("1234");
+    verifyOtp();
+    Keyboard.dismiss();
   };
 
-  // verifyOtp = async () => {
-  //   const { phoneNumber, to, _id, otp } = this.props.route.params;
-  //   console.log(otp, this.state.otp);
-  //   if (otp == this.state.otp && to == "detail") {
-  //     this.props.navigation.navigate("signup2", {
-  //       phoneNumber
-  //     });
-  //   } else if (otp == this.state.otp && to == "home") {
+  const verifyOtp = async () => {
+    const { phoneNumber, to, _id, otp } = route.params;
+    console.log(otp, value);
+    if (otp == value && to == "detail") {
+      navigation.navigate("signup2", {
+        phoneNumber
+      });
+    } else if (otp == value && to == "home") {
       // Toast.show({
       //   title: "Welcome",
       //   textBody: "Successfully Signed In",
       //   type: ALERT_TYPE.SUCCESS
       // });
-    //   await AsyncStorage.setItem("_id", _id);
-    //   console.log("success");
-    //   this.props.setAuth(_id);
-    // } else {
+      await AsyncStorage.setItem("_id", _id);
+      setAuth(_id);
+    } else {
       // Toast.show({
       //   title: "OTP",
       //   textBody: "Invalid OTP",
       //   type: ALERT_TYPE.DANGER
       // });
-  //     return;
-  //   }
-  // };
-    return (
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Ionicons
-            name="chevron-back"
-            size={22}
-            color={"black"}
-            style={styles.back}
-            onPress={() => {
-              this.props.navigation.pop();
+      return;
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Ionicons
+          name="chevron-back"
+          size={22}
+          color={"black"}
+          style={styles.back}
+          onPress={() => {
+            navigation.pop();
+          }}
+        />
+        <Text
+          style={{
+            color: "#191A1C",
+            fontSize: 20,
+            lineHeight: 28,
+            fontFamily: "p-500",
+            fontStyle: "normal"
+          }}
+        >
+          Verification
+        </Text>
+      </View>
+      <View style={styles.codeContainer}>
+        <Text
+          style={{
+            color: "#939497",
+            fontSize: 14,
+            lineHeight: 22,
+            fontFamily: "p-400",
+            fontStyle: "normal"
+          }}
+        >
+          Verify your account by entering 4 digit code.
+        </Text>
+        <View style={styles.codeBox}>
+          <CodeField
+            ref={ref}
+            {...props}
+            value={value}
+            onChangeText={setValue}
+            cellCount={CELL_COUNT}
+            rootStyle={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              width: "70%"
             }}
-          />
-          <Text
-            style={{
-              color: "#191A1C",
-              fontSize: 20,
-              lineHeight: 28,
-              fontFamily: "p-500",
-              fontStyle: "normal"
-            }}
-          >
-            Verification
-          </Text>
-        </View>
-        <View style={styles.codeContainer}>
-          <Text
-            style={{
-              color: "#939497",
-              fontSize: 14,
-              lineHeight: 22,
-              fontFamily: "p-400",
-              fontStyle: "normal"
-            }}
-          >
-            Verify your account by entering 4 digit code.
-          </Text>
-          <View style={styles.codeBox}>
-            <CodeField
-                ref={ref}
-                {...props}
-                value={value}
-                onChangeText={setValue}
-                cellCount={CELL_COUNT}
-                rootStyle={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                  width : "70%"
+            keyboardType="number-pad"
+            textContentType="oneTimeCode"
+            renderCell={({ index, symbol, isFocused }) =>
+              <Text
+                key={index}
+                style={{
+                  borderColor: isFocused ? "#2C9BCB" : "#E7E8EB",
+                  borderRadius: 8,
+                  borderWidth: isFocused ? 2 : 1,
+                  height: 56,
+                  width: 44,
+                  color: "#B3B4B7",
+                  lineHeight: 56,
+                  fontFamily: "p-500",
+                  textAlign: "center",
+                  fontSize: 18
                 }}
-                keyboardType="number-pad"
-                textContentType="oneTimeCode"
-                renderCell={({index, symbol, isFocused}) => (
-                  <Text
-                    key={index}
-                    style={{
-                        borderColor:  isFocused ? "#2C9BCB" : '#E7E8EB' ,
-                        borderRadius : 8,
-                        borderWidth:  isFocused ? 2 : 1,
-                        height: 56,
-                        width: 44,
-                        color : "#B3B4B7",
-                        lineHeight: 56,
-                        fontFamily: 'p-500',
-                        textAlign: 'center',
-                        fontSize: 18
-                    }}
-                    onLayout={getCellOnLayoutHandler(index)}>
-                    {symbol || (isFocused ? <Cursor /> : null)}
-                  </Text>
-                )}
-              />
-            {/* <OTPInputView
+                onLayout={getCellOnLayoutHandler(index)}
+              >
+                {symbol || (isFocused ? <Cursor /> : null)}
+              </Text>}
+          />
+          {/* <OTPInputView
               pinCount={4}
               // ref={ref => (this.otpInput = ref)}
-              code={this.state.otp}
+              code={value}
               onCodeChanged={val => {
                 this.setState({ otp: val });
               }}
@@ -168,71 +166,71 @@ const Verification = () => {
               autoFocusOnLoad={false}
               codeInputFieldStyle={styles.codecontent}
             /> */}
-          </View>
-          <View style={{ flexDirection: "row" }}>
-            <Text
-              style={{
-                color: "#191A1C",
-                fontSize: 14,
-                lineHeight: 22,
-                fontFamily: "p-400",
-                fontStyle: "normal"
-              }}
-            >
-              Don't Get Code?{" "}
-            </Text>
-            <Text
-              style={{
-                color: "#2C9BCB",
-                fontSize: 14,
-                lineHeight: 22,
-                fontFamily: "p-400",
-                fontStyle: "normal"
-              }}
-            >
-              {" "}Resend via Call
-            </Text>
-          </View>
         </View>
-        <TouchableOpacity
+        <View style={{ flexDirection: "row" }}>
+          <Text
+            style={{
+              color: "#191A1C",
+              fontSize: 14,
+              lineHeight: 22,
+              fontFamily: "p-400",
+              fontStyle: "normal"
+            }}
+          >
+            Don't Get Code?{" "}
+          </Text>
+          <Text
+            style={{
+              color: "#2C9BCB",
+              fontSize: 14,
+              lineHeight: 22,
+              fontFamily: "p-400",
+              fontStyle: "normal"
+            }}
+          >
+            {" "}Resend via Call
+          </Text>
+        </View>
+      </View>
+      <TouchableOpacity
+        style={{
+          ...styles.startBtn,
+          backgroundColor: "#1FBDBA"
+        }}
+        // disabled={value.length < 4}
+        onPress={() => {
+          verifyOtp();
+        }}
+      >
+        <Text style={styles.btnText}>Verify</Text>
+      </TouchableOpacity>
+      <View style={loginStyle.note}>
+        <Text
           style={{
-            ...styles.startBtn,
-            backgroundColor: "#1FBDBA"
-          }}
-          // disabled={this.state.otp.length < 4}
-          onPress={() => {
-            this.verifyOtp();
+            fontFamily: "p-500",
+            color: "#000"
           }}
         >
-          <Text style={styles.btnText}>Verify</Text>
-        </TouchableOpacity>
-        <View style={loginStyle.note}>
+          Already member?
+        </Text>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("login");
+          }}
+        >
           <Text
             style={{
               fontFamily: "p-500",
-              color: "#000"
+              color: "#1FBDBA",
+              marginLeft: 3
             }}
           >
-            Already member?
+            Log in
           </Text>
-          <TouchableOpacity
-            onPress={() => {
-              this.props.navigation.navigate("login");
-            }}
-          >
-            <Text
-              style={{
-                fontFamily: "p-500",
-                color: "#1FBDBA",
-                marginLeft: 3
-              }}
-            >
-              Log in
-            </Text>
-          </TouchableOpacity>
-        </View>
+        </TouchableOpacity>
       </View>
-    );
-  }
-export default Verification
+    </View>
+  );
+};
+export default Verification;
 //export default connect(null, { setAuth })(Verification);
